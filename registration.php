0c8e057166a981e6f05f8d04f6427db35209e214
Aioane Mihaita Adi <Mihaita.Aioane@marconi.loc>
2016-05-23 15:57:50 +0200

    Update mirror fix for the test page
    -increment security on registration php;
    -test.html change the name of the variable.

--- registration.php
+++ registration.php
@@ -3,37 +3,28 @@ $servername = "localhost:3307";
 $username = "root";
 $password = "usbw";
 $dbname = "websitevpn";
-if (isset($_REQUEST["name"])) 
+if (!empty($_REQUEST["name"]) and (!empty($_REQUEST['lastname'])) and (!empty($_REQUEST['age']))) 
 { 
-          $name = $_REQUEST['name'];
-}
-else echo 'Variabile nome non esiste!';
-if (isset($_REQUEST['lastname'])) 
-{ 
-          $lastname = $_REQUEST['lastname'];
-}
-else echo 'Variabile Cognome non esiste!'; 
-if (isset($_REQUEST['age'])) 
-{ 
-          $age = $_REQUEST['age'];
-}
-else echo 'Variabile Eta non esiste!'; 
+    $name = $_REQUEST['name'];
+    $lastname = $_REQUEST['lastname'];
+    $age = $_REQUEST['age'];
+            
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error()); }
 
+    $sql = "INSERT INTO USERS VALUES (NULL, $name, $lastname, $age)";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully"; } 
+    else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn); }
+
+    mysqli_close($conn);
 }
-
-$sql = "INSERT INTO USERS VALUES (NULL, $name, $lastname, $age)";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
+else
+    echo  "<script type='text/javascript'>alert('Hai lasciato qualche campo vuoto, completa tutti i campi! Ritorna sulla pagina di test e riprova che qualcosa è andato storto!');</script>"; 
 ?>
